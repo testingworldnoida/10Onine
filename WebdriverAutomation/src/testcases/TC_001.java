fb20b6a7ba6e977746560f67a1e026081d0ff59a
testingworldnoida <testingworldnoida@gmail.com>
2019-03-08 22:34:55 +0530

    Added New test cases

--- WebdriverAutomation/src/testcases/TC_001.java
+++ WebdriverAutomation/src/testcases/TC_001.java
@@ -10,11 +10,12 @@ public class TC_001 {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\TestingWorld\\Downloads\\chromedriver_win32 (6)\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementByName("pass").sendKeys("Hello");
 		driver.findElementByXPath("//input[@type='submit']").click();
 		driver.close();
+		System.out.println("Hello");
 	}
 	
 }
